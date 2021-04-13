f89fb28da9c790dc95d9cb3e850d9559911a8829
Gabriel Roger <gabriel.nroger@gmail.com>
2021-04-12 21:00:37 -0300

    Register publication admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from blog.models import Author
+from blog.models import Author, Publication
 
 
 class AuthorAdmin(admin.ModelAdmin):
     pass
 
 
+class PublicationAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(Publication, PublicationAdmin)
