8670e8fc897689cf8c4a68c6ac94088d5df1e43b
Gabriel Roger <gabriel.nroger@gmail.com>
2021-04-15 11:24:24 -0300

    Add render view to all templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Publication
+
+
+def render_index(request):
+    return render(request, 'index.html', {})
+
+
+def render_publication_done(request):
+    publication_data = {
+        'title': request.POST.get('title'),
+        'preview_description': request.POST.get('preview_description'),
+        'description': request.POST.get('description'),
+        'external_article': request.POST.get('external_article'),
+        'image': request.POST.get('image'),
+        'author': request.POST.get('author')
+    }
+    publication = Publication(**publication_data)
+    publication.save()
+    return render(request, 'publication_done.html', {'title': publication.title})
